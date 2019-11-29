19486367d733b207ced20fee4c0fdccc638662d8
byulparan <byul@byulparanui-MacBook-Air.local>
2019-11-29 17:21:45 +0900

    fix `pp` and `bpm` for client-side

--- client-side.lisp
+++ client-side.lisp
@@ -12,34 +12,24 @@
 	    (line.kr 0 0 (+ lag 1) :act :free)
 	    [(impulse.kr 30) tempo])
 	(lambda (tempo)
-	  (clock-bpm (* 60 (reciprocal tempo)))))
+	  (clock-bpm (* 60 (reciprocal tempo))))
+	*counter-group*)
       (metro bpm :relaunch relaunch :lag lag)
       (dolist (f *bpm-functions*)
 	(funcall f bpm :relaunch relaunch :lag lag)))))
 
-(defmacro pp (arg &body body)
+(defmacro pp (&body body)
   (let* ((sym-beat (alexandria:symbolicate "BEAT"))
-	 (sym-dur (alexandria:symbolicate "DUR")))
-    (alexandria:with-gensyms (name)
-      `(let* ((,name ,arg))
-	 (if (numberp ,name) (let ((,sym-beat (+ ,sym-beat ,name)))
-			       (at-beat ,sym-beat
-				 (let* ((,name ,(car body)))
-				   (apply (if (keywordp ,name) #'ctrl #'synth)
-					  ,name
-					  (list
-					   ,@(progn (alexandria:if-let ((dur (getf (cdr body) :dur)))
-						      (append
-						       (alexandria:remove-from-plist (cdr body) :dur)
-						       (list :dur `(clock-dur ,dur)))
-						      (append (cdr body) (list :dur `(clock-dur ,sym-dur))))))))))
-	   (at-beat ,sym-beat
-	     (apply (if (keywordp ,name) #'ctrl #'synth) ,name
-		      (list
-		       ,@(progn (alexandria:if-let ((dur (getf body :dur)))
-				 (append (alexandria:remove-from-plist body :dur)
-					 (list :dur `(clock-dur ,dur)))
-				 (append body (list :dur `(clock-dur ,sym-dur)))))))))))))
+	 (sym-dur (alexandria:symbolicate "DUR"))
+	 (has-beat-offset (evenp (length body)))
+	 (params (if has-beat-offset (cddr body) (cdr body)))
+	 (has-dur-p (getf params :dur)))
+    (when has-dur-p (setf params (alexandria:remove-from-plist params :dur)))
+    (if has-beat-offset 
+	`(let ((,sym-beat (+ ,sym-beat ,(car body))))
+	   (at-synth ,sym-beat ,(second body) ,@params
+		     :dur (clock-dur ,(if has-dur-p has-dur-p sym-dur))))
+      `(at-synth ,sym-beat ,(car body) ,@params :dur (clock-dur ,(if has-dur-p has-dur-p sym-dur))))))
 
 (defmacro next-dur (beat new-value)
   (let ((sym-beat (alexandria:symbolicate "BEAT"))
@@ -54,9 +44,9 @@
 	((listp n) (alexandria:random-elt n))))
 
 (defmacro sinr (offset gain ratio)
-  `(+ ,offset (* ,gain (sin (* 2 pi ,(alexandria:symbolicate "BEAT") ,ratio)))))
+  `(+ ,offset (* ,gain (sin (* pi ,(alexandria:symbolicate "BEAT") ,ratio)))))
 
 (defmacro cosr (offset gain ratio)
-  `(+ ,offset (* ,gain (cos (* 2 pi ,(alexandria:symbolicate "BEAT") ,ratio)))))
+  `(+ ,offset (* ,gain (cos (* pi ,(alexandria:symbolicate "BEAT") ,ratio)))))
 
 
